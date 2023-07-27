08b6b04d06e63ead11ae4856bb40f7e50e00f1b8
DavidM444 <david541gamer@gmail.com>
2023-07-26 19:43:47 -0500

    Home page

---
+++ src/components/Home.vue
@@ -0,0 +1,221 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+                <div class="col-sm-8 text-md-left d-flex logo ">
+                    <img class="" src="../assets/img/T.png" alt="">
+                    <h1 class="align-self-center">QualityTex</h1>
+                </div>
+
+                <div class="col-sm-4 text-center align-self-center mt-sm-0 mt-3 mb-sm-0 mb-3">
+                    <RouterLink class="btn btn-primary link" to="/">Inciar Sesion</RouterLink>
+                    <RouterLink class="btn btn-success link" to="/singup">Registrarse</RouterLink>
+                </div>
+
+            
+
+        </div>
+        <div class="row bg-warning">
+            <div>
+                <h2>Estandares de calidad textil</h2>
+                <p>Los estándares de calidad en la industria textil son criterios fundamentales para garantizar la
+                    excelencia en
+                    los productos y servicios. Estos estándares se establecen con el propósito de asegurar que los textiles
+                    cumplan con requisitos específicos y satisfagan las necesidades de los consumidores.
+                    Estos estándares de calidad en textiles incluyen diversos aspectos, tales como la resistencia del
+                    tejido, la
+                    solidez del color, la uniformidad del acabado, y la durabilidad frente al desgaste y lavado. Además, se
+                    toman en cuenta factores como la ausencia de defectos, la precisión en las dimensiones y la seguridad
+                    del
+                    producto.
+                    La certificación de calidad más reconocida en la industria textil es el Sistema de Certificación
+                    Oeko-Tex®,
+                    el cual evalúa si los productos textiles cumplen con los límites establecidos para sustancias nocivas y
+                    químicos peligrosos. Esto asegura que los textiles sean seguros para la salud humana y el medio
+                    ambiente.
+                    Asimismo, los estándares de calidad también abarcan el proceso de producción, desde la selección de las
+                    materias primas hasta la confección y el empaquetado, cada etapa debe seguir protocolos que garanticen
+                    la
+                    calidad del producto final y cuenten con los requisitos establecidos.</p>
+            </div>
+            <div>
+
+                <h2>Innovaciones En el ambito textil</h2>
+
+                <p>
+
+                    Las innovaciones en textiles han revolucionado la industria, introduciendo nuevos materiales y
+                    tecnologías que
+                    mejoran la calidad y funcionalidad de los productos textiles. Estas novedades han impactado en diversas
+                    áreas,
+                    desde la moda hasta la medicina y la ingeniería. A continuación, veremos algunas de las principales
+                    innovaciones que están transformando el mundo de los textiles. <br>
+                <ul>
+                    <li><strong>Textiles tecnicos</strong>
+                        <p>
+                            los textiles técnicos han sido una de las mayores innovaciones en la industria. Estos
+                materiales se diseñan específicamente para satisfacer necesidades técnicas y funcionales. Por ejemplo,
+                los
+                textiles resistentes al fuego se utilizan en la protección laboral y en la industria aeroespacial,
+                mientras que
+                los textiles impermeables son ideales para prendas de lluvia y equipamiento deportivo.
+                        </p>
+                    </li>
+                    <li>
+                        <strong>Implementacion de tecnologia</strong>
+                <p>Otra innovación emocionante es el desarrollo de textiles inteligentes, que incorporan electrónica y
+                sensores
+                para proporcionar funcionalidades avanzadas. Los textiles inteligentes pueden monitorear signos vitales
+                en la
+                ropa deportiva, adaptar la temperatura en prendas térmicas o incluso tener integrados sistemas de
+                iluminación
+                para aplicaciones de moda.</p>
+                    </li>
+                    <li>
+                        <strong>Sostenibilidad textil</strong>
+                <p>La sostenibilidad también ha sido un foco importante de innovación en textiles. El desarrollo de fibras
+                y
+                tejidos a partir de materiales reciclados o de fuentes renovables ha ganado popularidad. Estos textiles
+                ecológicos reducen el impacto ambiental de la industria y contribuyen a un futuro más sostenible.</p>
+                    </li>
+                    <li>
+                        
+                <strong>Impresion textil</strong>
+                <p>Además, las tecnologías de impresión textil han avanzado significativamente. La impresión digital
+                permite
+                estampar diseños complejos y personalizados sobre tejidos de manera más eficiente, lo que ha impulsado
+                la
+                creatividad en la moda y en la decoración del hogar.</p>
+                    </li>
+
+                    <li>
+                        
+                <strong>Textiles biomedicos</strong>
+                <p>En el ámbito médico, los textiles biomédicos son otra innovación destacada. Estos textiles se utilizan
+                en
+                aplicaciones médicas, como vendajes avanzados que ayudan en la cicatrización de heridas o textiles que
+                evitan
+                infecciones en entornos hospitalarios.</p>
+                    </li>
+                    <li>
+                        
+                <strong>nanotecnología</strong>
+
+                <p>La nanotecnología ha abierto un mundo de posibilidades en la mejora de las propiedades de los
+                textiles. Con nanofibras y nanorecubrimientos, los textiles pueden obtener características
+                superhidrofóbicas,
+                autorreparación y protección contra bacterias y otros agentes dañinos.</p>
+                    </li>
+                </ul>
+                
+
+                
+
+              
+
+               
+
+
+
+                </p>
+            </div>
+            <div>
+                <h2>Calidad de confección</h2>
+                <p>
+                    La calidad de confección en la industria textil es un aspecto fundamental para garantizar la
+                    satisfacción del
+                    cliente y la durabilidad de los productos. Una buena calidad de confección no solo se refiere a la
+                    apariencia
+                    estética de una prenda, sino también a su resistencia y comodidad durante el uso. A continuación,
+                    exploraremos
+                    la importancia de la calidad de confección y los elementos clave que influyen en ella. <br>
+
+                    Uno de los pilares de la calidad de confección es la precisión en los detalles y acabados. Las costuras
+                    bien
+                    cosidas y los remates limpios son indicadores de una prenda bien elaborada. Los bordes sin deshilacharse
+                    y los
+                    dobladillos bien hechos son signos de atención al detalle y cuidado en el proceso de confección. <br>
+
+                    Otro aspecto crucial es el uso de materiales adecuados. Los textiles de calidad, combinados con los
+                    hilos y
+                    accesorios apropiados, aseguran que la prenda mantenga su forma y resistencia a lo largo del tiempo. Un
+                    buen
+                    equilibrio entre la selección de materiales y el proceso de producción es esencial para lograr una
+                    calidad
+                    óptima. <br>
+
+                    Además, la calidad de la confección se relaciona con el ajuste y la funcionalidad de la prenda. Las
+                    prendas bien
+                    diseñadas y confeccionadas deben adaptarse correctamente al cuerpo, permitiendo la libertad de
+                    movimiento y
+                    evitando restricciones incómodas. Los cierres, botones y cremalleras deben ser duraderos y funcionales,
+                    facilitando el uso y prolongando la vida útil de la prenda. <br>
+
+                    El control de calidad durante todo el proceso de producción es esencial para garantizar la excelencia en
+                    la
+                    confección. Inspecciones regulares y pruebas de calidad ayudan a identificar posibles problemas y a
+                    corregirlos
+                    antes de que las prendas lleguen al mercado.
+
+                    La calidad de confección no solo se aplica a la moda, sino también a otros productos textiles, como ropa
+                    de
+                    cama, toallas y cortinas. En estos casos, la durabilidad y resistencia son factores clave para asegurar
+                    que los
+                    productos mantengan su aspecto y rendimiento a lo largo del tiempo.</p>
+            </div>
+
+        </div>
+
+
+
+
+    </div>
+    <footer class="bg-light py-4" slot="foter">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-left">
+                    <p>&copy; 2023 QualityTex. Todos los derechos reservados.</p>
+                    <p>Dirección: Calle 23 #23-11 123, Bogota, Colombia</p>
+                    <p>Email: qualitytext22@hotmail.com</p>
+                </div>
+                <div class="col-md-6 text-center text-md-right">
+                    <p>Síguenos en redes sociales:</p>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="#">Facebook</a></li>
+                        <li class="list-inline-item"><a href="#">Twitter</a></li>
+                        <li class="list-inline-item"><a href="#">Instagram</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>ç
+
+</template>
+<style scoped lang="scss">
+.link{
+    margin-right: 1rem;
+}
+
+
+.bt {
+    margin-left: auto;
+}
+
+.logo img {
+    max-width: 8em;
+    height: auto;
+    border-radius: 20%;
+    padding: 0.5rem;
+    
+
+}
+
+.logo h1 {
+    color: chartreuse;
+    margin: auto;
+}
+</style>
+<script>
+export default {
+    name: 'Home'
+}
+</script>
