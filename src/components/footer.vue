fb657f6fd9e5d0710155b3a107febddfc0a19615
DavidM444 <david541gamer@gmail.com>
2023-07-26 19:44:05 -0500

    footer component

---
+++ src/components/footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="ft">
+        <footer class="bg-warning py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-left">
+                    <p>&copy; 2023 QualityTex. Todos los derechos reservados.</p>
+                    <p>Dirección: Calle 23 #23-11 123, Bogota, Colombia</p>
+                    <p>Email: qualitytext22@hotmail.com</p>
+                </div>
+                <div class="col-md-6 text-center text-md-right">
+                    <p>Síguenos en redes sociales:</p>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="#">Facebook</a></li>
+                        <li class="list-inline-item"><a href="#">Twitter</a></li>
+                        <li class="list-inline-item"><a href="#">Instagram</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    </div>
+</template>
+<script>
+export default{
+    name: 'Footer'
+}
+</script>
+<style scoped>
+.bg-warning{
+    background-color: #6e6e2e;
+}
+</style>
