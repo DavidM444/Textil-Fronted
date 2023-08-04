624e7a620bc5142035f4664ba3743e2829fe7a0e
DavidM444 <david541gamer@gmail.com>
2023-08-04 17:07:11 -0500

    theme registro

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,19 +1,19 @@
 <template>
    
         <div class="container-fluid d-flex SignUp">
-            <div class="col d-none d-sm-block">
-                <img class="float" src="../assets/img/moonstarious-project-KbaSNX-6Vdo-unsplash.jpg" alt="" style="height: 44em; width: 80%" >
+            <div class="col d-none d-sm-block flt">
+                <img  src="../assets/img/moonstarious-project-KbaSNX-6Vdo-unsplash.jpg" alt="" >
             </div>
             <div class="col"> 
                 <div class="row">
                     <div class="container-fluid pt-3">
                         <div class="d-flex logo">
                             <img class="img-fluid mx-3" src="../assets/img/T.png" alt="">
-                            <h1 class="mt-2">QualityTex</h1>
+                            <h1 class="mt-2 text-center">Quality Tex</h1>
                         </div>
                     </div>
                 </div>
-                <section class="mt-3 Sp te">
+                <section class="mt-5 reg">
                     <h2 class="text-center">Registrarse</h2>
                     <form class="mb-3">
                         <label for="name" class="form-label">Nombre</label>
@@ -25,16 +25,11 @@
                         <label for="" class="form-label">Numero Telefonico</label>
                         <input type="text" class="form-control" pattern="[1-9]\d*" required name="" id="" aria-describedby="helpId" placeholder="">
                         
-                        <label for="" class="form-label">Fecha de Nacimiento</label>
-                        <input type="date" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-                    
-                        <label for="Cargo" class="form-label">Cargo</label>
-                        <select class="form-select form-select-lg" name="" id="">
-                            <option selected>Gestor de calidad</option>
-                            <option value="">Gerente</option>
-                            
-                        </select>
-                        <button type="submit" class="btn btn-primary mt-3">Submit</button>
+                        
+                        <label for="Email" class="form-label">Email</label>
+                        <input type="email" class="form-control" required name="Email" id="Email" aria-describedby="helpId" placeholder="">
+  
+                        <button type="submit" class="btnr btn btn-primary mt-3">Submit</button>
                         
                     </form>
 
@@ -59,9 +54,31 @@ export default{
 </script>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+$bg-ss: #00473e;
+$bg2:#f2f7f5;
+
+
 .SignUp{
-    background-color: rgb(205, 244, 227);
+    background-color: $bg2;
+}
+.flt{
+    img{
+        height: 38em;
+        width: 90%;
+    }
+}
+.btnr{
+    background-color: $bg-ss;
+    border-bottom: #00473e;
+}
+.btnr:hover{
+    background-color: #0d6efd;
+    font-size: large;
+}
+.reg h2{
+    color: #00473e;
 }
 
 
