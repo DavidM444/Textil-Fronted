aef1b3aa01a834383bc680dd99b299229d60daa8
DavidM444 <david541gamer@gmail.com>
2023-12-01 19:47:17 -0500

    chart js implemnt

---
+++ src/components/Appgrafico.vue
@@ -0,0 +1,14 @@
+<template>
+
+  
+    <BarChart />
+  </template>
+  
+  <script>
+  import BarChart from './Grafico.vue'
+  
+  export default {
+    name: 'Appgrafico',
+    components: { BarChart }
+  }
+  </script>
