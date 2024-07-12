e04e5e1b125aed1cd9ca2bcd0a19a9ecaae0411f
DavidM444 <david541gamer@gmail.com>
2024-07-12 12:00:53 -0500

    Disposcion de espacio para el footer.

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,13 +2,13 @@
     <div class="ft">
         <footer class="bg-f py-4">
         <div>
-            <div class="row">
-                <div class="col-md-6 text-center text-md-left">
+            <div class="row text-center justify-content-center">
+                <div class="col-md-4 text-md-left">
                     <p>&copy; 2023 QualityTex. Todos los derechos reservados.</p>
                     <p>Dirección: Calle 23 #23-11 123, Bogota, Colombia</p>
                     <p>Email: qualitytext22@hotmail.com</p>
                 </div>
-                <div class="col-md-6 text-center text-md-right">
+                <div class="col-md-4 text-md-right">
                     <p>Síguenos en redes sociales:</p>
                     <ul class="list-inline">
                         <li class="list-inline-item"><a href="#">Facebook</a></li>
